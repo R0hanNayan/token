15fac35b3bbab37b296d9ad69ca0fa280acdd1bf
RohanNayan <rohannayan405@gmail.com>
2024-03-03 16:23:24 +0530

    Authentication Using Internet Identity

--- src/token_assets/src/index.jsx
+++ src/token_assets/src/index.jsx
@@ -1,8 +1,27 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import App from "./components/App";
+import { AuthClient } from "@dfinity/auth-client";
 
-const init = async () => { 
+const init = async () => {
+  //To Authenticate user using Internet Identity:
+  const authClient = await AuthClient.create();
+
+  //To prevent repeated login pop up
+  if (await authClient.isAuthenticated()) {
+    handleAuthentication(authClient)
+  } else {
+    await authClient.login({
+      identityProvider: "https://identity.ic0.app/#authorize",
+      onSuccess: () => {
+        handleAuthentication(authClient);
+      }
+    });
+  }
+
+}
+
+async function handleAuthentication(authClient) {
   ReactDOM.render(<App />, document.getElementById("root"));
 }
 
